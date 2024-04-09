1c3885b0e6a403d920cf1c36aec7ca54f95e1b19
Kithru2007 <163752619+Kithru2007@users.noreply.github.com>
2024-04-09 21:43:30 +0530

    finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,20 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import PaymentCreateForm from './components/PaymentCreateForm';
 import PaymentAll from './components/PaymentAll';
 import PaymentUpdateForm from './components/PaymentUpdateForm';
-// import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
-      {/* <NavBar/> */}
       <div className='pages'>
       <Routes>
 
           <Route path='/create' element={<PaymentCreateForm/>}/>
           <Route path='/' element={<PaymentAll/>}/>
           <Route path='/update/:id' element={<PaymentUpdateForm/>}/>
+          
 
       </Routes>
       </div>
